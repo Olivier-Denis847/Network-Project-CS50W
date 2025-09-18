2c47e906e4affb7c957e82722a3d97238810857f
Olivier-Denis847 <olivierphilippedenis@gmail.com>
2025-09-17 22:36:17 -0400

    Updated the front end and improved code organization

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -6,49 +6,56 @@ export function view_posts(user = null, follows = false){
     .then(data => {
         data.forEach(post => {
             const element = document.createElement('div');
-            element.classList.add('post')
+            element.classList.add('post');
+            element.id = `post_${post.post_id}`;
  
             let name_header = `<h3>${post.creator}</h3>`;
             if (src == 'index'){
                 name_header = `<a href = "/profile/${post.creator_id}">${name_header}</a>`;
             };
-            let like_id = `like${post.post_id}`;
+            let like_type = `like`;
             if (post.is_liked){
-                like_id = 'dis' + like_id;
+                like_type = 'dislike';
             };
 
             element.innerHTML = `${name_header}
             <p style="font-weight: lighter;">${post.posted}</p>
             <p>${post.content}</p>
-            <div class = "like_button"><p id="${like_id}">${post.likes}</p></div>`;
+            <div class = "like_button ${like_type}"><img src="static/images/heart.svg">
+            <span>${post.likes}</span></div>`;
 
-            element.querySelector('.like_button').addEventListener('click', () => {
-                fetch(`/add_like`, {
-                    method : 'PUT',
-                    body : JSON.stringify({post_id : post.post_id, is_liked : post.is_liked}),
-                    headers : {
-                        'X-CSRFToken': CSRF_TOKEN,
-                        'Content-Type': 'application/json'
-                    }
-                })
-                .then(response => response.json())
-                .then(result => {
-                    console.log(result);
-                    const button_content = element.querySelector('.like_button').querySelector('p');
-                    const button_state = button_content.id.substring(0,3);
-                    if (button_state == 'dis'){
-                        button_content.id = 'like' + post.post_id;
-                        button_content.innerHTML = parseInt(button_content.innerHTML) - 1;
-                    }
-                    else{
-                        button_content.id = 'dislike' + post.post_id;
-                        button_content.innerHTML = parseInt(button_content.innerHTML) + 1;
-                    }
-                    post.is_liked = !post.is_liked;
-                });
-            });
+            element.querySelector('.like_button').addEventListener('click', () => like_button(element, post));
             document.querySelector('#post_list').append(element);
         });
+    });
+}
+
+export function like_button(element, post){
+    fetch(`/add_like`, {
+        method : 'PUT',
+        body : JSON.stringify({post_id : post.post_id, is_liked : post.is_liked}),
+        headers : {
+            'X-CSRFToken': CSRF_TOKEN,
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(result => {
+        console.log(result);
+
+        const button = element.querySelector('.like_button');
+        const button_content = button.querySelector('span');
+        if (button.classList[1] == 'dislike'){
+            button.classList.remove('dislike');
+            button.classList.add('like');
+            button_content.innerHTML = parseInt(button_content.innerHTML) - 1;
+        }
+        else{
+            button.classList.remove('like');
+            button.classList.add('dislike');
+            button_content.innerHTML = parseInt(button_content.innerHTML) + 1;
+        }
+        post.is_liked = !post.is_liked;
     });
 }
 
